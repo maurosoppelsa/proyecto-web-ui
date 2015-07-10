f98aea87ef3fdf1f4cef72ff6f0a76979eb0aa59
mauro soppelsa <mauro_soppelsa@hotmail.com>
2015-07-10 17:47:39 -0300

    ultimos cambios

--- js/button_efects.js
+++ js/button_efects.js
@@ -1,1 +1,14 @@
 
+$('#main_BT').hover(function(){
+
+$('#main_BT span').slideUp();
+
+$('#main_BT img').slideDown();
+
+}).mouseleave(function(){
+
+  $('#main_BT span').slideDown();
+
+  $('#main_BT img').slideUp();
+
+});
