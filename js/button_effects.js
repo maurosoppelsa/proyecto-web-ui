5d7909aa278e14a3e6b206e7982957260e78133a
mauro soppelsa <mauro_soppelsa@hotmail.com>
2015-07-28 18:03:20 -0300

    feet

--- js/button_effects.js
+++ js/button_effects.js
@@ -21,4 +21,57 @@ $('#main_BT').click(function(){
 
   $(this).fadeOut(1000);
 
+  move_your_feet()
+
 });
+
+function move_your_feet(){
+
+    setTimeout(function () {
+
+      $('#foot1').fadeIn();
+
+    }, 2000);
+
+    setTimeout(function () {
+
+      $('#foot2').fadeIn();
+
+    }, 3000);
+
+    setTimeout(function () {
+
+      $('#foot3').fadeIn();
+
+    }, 4000);
+
+    setTimeout(function () {
+
+      $('#foot4').fadeIn();
+
+    }, 5000);
+
+    setTimeout(function () {
+
+      $('#foot5').fadeIn();
+
+    }, 6000);
+
+    setTimeout(function () {
+
+      $('#foot6').fadeIn();
+
+    }, 7000);
+
+    setTimeout(function () {
+
+      $('#foot7').fadeIn();
+
+    }, 8000);
+
+    setTimeout(function () {
+
+      $('#foot8').fadeIn();
+
+    }, 9000);
+}
